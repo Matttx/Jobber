2145858fb5298984eee73631737ce88169d84b9d
mattéo <fauchonmatteo@gmail.com>
2022-04-15 16:04:22 +0200

    refactor: constants

--- Jobber/Constants.swift
+++ Jobber/Constants.swift
@@ -11,7 +11,7 @@ let kAPIAccessTokenURL = "https://entreprise.pole-emploi.fr/connexion/oauth2/acc
 
 enum Credentials {
     static let grant_type = "client_credentials"
-    static let client_id = "PAR_jobber_a4e1540e65865a3c7ebe590c4d77f745036856c323e894d7324c5361e41e8e80"
-    static let client_secret = "ac2a5a23b07ef1493d0936f9489069cc120797226cdcc0c73f420e63c7046c3b"
-    static let scope = "application_PAR_jobber_a4e1540e65865a3c7ebe590c4d77f745036856c323e894d7324c5361e41e8e80 api_offresdemploiv2 o2dsoffre"
+    static let client_id = "XXXXXXX" // Your Pôle-emploi client ID
+    static let client_secret = "XXXXXXX" // Your Pôle-emploi client secret
+    static let scope = "application_[YOUR_CLIENT_ID] api_offresdemploiv2 o2dsoffre"
 }
